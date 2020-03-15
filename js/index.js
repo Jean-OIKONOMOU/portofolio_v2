736374cdf5e4ba28c169b2174532dd98febec013
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2020-03-15 21:58:40 +0100

    fixed navbar issue

--- js/index.js
+++ js/index.js
@@ -11,3 +11,16 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+window.onscroll = function() { myFunction() };
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
