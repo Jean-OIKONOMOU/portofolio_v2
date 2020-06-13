00603e0bcdbc11acc2f66bea6dd21d90bfb91276
Jean-Adrian OIKONOMOU <Jean.oikonomou@gmail.com>
2020-06-13 20:26:19 +0200

    tryouts for navbar

--- js/index.js
+++ js/index.js
@@ -12,18 +12,18 @@ navLinks.forEach(link => {
     })
 })
 
-window.onscroll = function() { myFunction() };
+//window.onscroll = function() { myFunction() };
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+//var header = document.getElementById("myHeader");
+//var sticky = header.offsetTop;
 
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
+//function myFunction() {
+//    if (window.pageYOffset > sticky) {
+//        header.classList.add("sticky");
+//    } else {
+//        header.classList.remove("sticky");
+//    }
+//}
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
